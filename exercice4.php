766c5b498ebbe855923e99916747c70cc46d8a0a
biscuitsaler <durandmallory@live.fr>
2017-01-18 16:14:22 +0100

    rectif int age

--- exercice4.php
+++ exercice4.php
@@ -68,9 +68,9 @@ class Chat
     }
 }
     // je définis mes 3 chats avec les informations
-    $monChat1 = new Chat("jacky","3","blanc","male","siamois");
-    $monChat2 = new Chat("sushi","5","noir","femelle","persan");
-    $monChat3 = new Chat("garfield","2","roux","male","ocicat");
+    $monChat1 = new Chat("jacky",3,"blanc","male","siamois");
+    $monChat2 = new Chat("sushi",5,"noir","femelle","persan");
+    $monChat3 = new Chat("garfield",2,"roux","male","ocicat");
 
     
     echo "Bonjour je suis" . get_class($monChat1). "  " . $monChat ->prenom() . "et j'ai" . $monChat ->age() . "je suis" . $monChat ->couleur() . "de sexe" . $monChat ->sexe() . "et de race" . $monChat ->race();
